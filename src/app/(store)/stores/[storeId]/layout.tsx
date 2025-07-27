f3ef33ea7aebe0592b132aa382f27629ac77e7a7
seojunhwan <seojunhwan@kakao.com>
2025-07-27 17:46:47 +0900

    feat: 가게 상세 페이지에 Bleed 컴포넌트 추가 및 GNB 레이아웃 수정

--- src/app/(store)/stores/[storeId]/layout.tsx
+++ src/app/(store)/stores/[storeId]/layout.tsx
@@ -3,6 +3,7 @@
 "use client";
 
 import ChevronLeftIcon from "@/assets/chevron-left.svg";
+import { Bleed } from "@/components/ui/Bleed";
 import { GNB } from "@/components/ui/GNB";
 
 import * as styles from "./layout.css";
@@ -14,14 +15,17 @@ export default function StoreDetailLayout({
 }) {
   return (
     <div className={styles.storeDetailLayout}>
-      <GNB
-        leftAddon={
-          <button style={{ display: "flex", alignItems: "center" }}>
-            <ChevronLeftIcon width={24} height={24} />
-          </button>
-        }
-        rightAddon={<button>대충 공유하기 버튼</button>}
-      />
+      <Bleed inline={20}>
+        {/* TODO: GNB fixed 여쭤보기 */}
+        <GNB
+          leftAddon={
+            <button style={{ display: "flex", alignItems: "center" }}>
+              <ChevronLeftIcon width={24} height={24} />
+            </button>
+          }
+          rightAddon={<button>대충 공유하기 버튼</button>}
+        />
+      </Bleed>
 
       {children}
     </div>
