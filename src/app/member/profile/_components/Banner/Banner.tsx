fbf0153d13465a9cfdb0cb9608d992417a462956
seojunhwan <seojunhwan@kakao.com>
2025-08-01 18:51:24 +0900

    feat: Banner 컴포넌트의 링크 구조 변경 및 스타일 수정

--- src/app/member/profile/_components/Banner/Banner.tsx
+++ src/app/member/profile/_components/Banner/Banner.tsx
@@ -12,26 +12,32 @@ import * as styles from "./Banner.css";
 
 export const Banner = () => {
   return (
-    <Bleed inline={20} className={styles.wrapper}>
-      <HStack align='center' gap={8}>
-        <Image
-          src='/images/store.png'
-          alt='가게 이미지'
-          width={55}
-          height={55}
-        />
-        <VStack gap={4} justify='center'>
-          <Text typo='body1Sb' color='neutral.10'>
-            당신의 소중한 가게를 소개해주세요
-          </Text>
-          <Link href='/stores/register' className={styles.linkWrapper}>
-            <Text typo='label2Sb' color='neutral.10'>
-              가게 등록하기
+    <Link href='/stores/register' className={styles.linkWrapper}>
+      <Bleed inline={20} className={styles.wrapper}>
+        <HStack align='center' gap={8}>
+          <Image
+            src='/images/store.png'
+            alt='가게 이미지'
+            width={55}
+            height={55}
+          />
+          <VStack gap={4} justify='center'>
+            <Text typo='body1Sb' color='neutral.10'>
+              당신의 소중한 가게를 소개해주세요
             </Text>
-            <ChevronRightIcon width={16} height={16} className={styles.icon} />
-          </Link>
-        </VStack>
-      </HStack>
-    </Bleed>
+            <HStack gap={8} align='center'>
+              <Text typo='label2Sb' color='neutral.10'>
+                가게 등록하기
+              </Text>
+              <ChevronRightIcon
+                width={16}
+                height={16}
+                className={styles.icon}
+              />
+            </HStack>
+          </VStack>
+        </HStack>
+      </Bleed>
+    </Link>
   );
 };
