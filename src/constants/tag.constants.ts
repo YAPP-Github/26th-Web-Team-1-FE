2a669bf7be139de783b6a13e5a35d6285f0daa1c
seojunhwan <seojunhwan@kakao.com>
2025-08-17 20:56:37 +0900

    feat: 새로운 태그 상수 파일 추가 (ATMOSPHERE_TAGS, UTILITY_TAGS)

---
+++ src/constants/tag.constants.ts
@@ -0,0 +1,82 @@
+export const ATMOSPHERE_TAGS = [
+  {
+    iconUrl: "/images/register/tag-atmosphere-1.png",
+    name: "OLD_STORE_MOOD",
+    label: "노포 감성",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-2.png",
+    name: "ENERGETIC",
+    label: "활기찬",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-3.png",
+    name: "INSTAGRAMMABLE",
+    label: "인스타 감성",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-4.png",
+    name: "QUIET",
+    label: "조용한",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-5.png",
+    name: "GOOD_FOR_DRINKING",
+    label: "술 땡기는",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-6.png",
+    name: "GOOD_FOR_DATING",
+    label: "데이트하기 좋은",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-7.png",
+    name: "GOOD_FOR_FAMILY",
+    label: "부모님과 가기 좋은",
+  },
+  {
+    iconUrl: "/images/register/tag-atmosphere-8.png",
+    name: "YOUTUBE_FAMOUS",
+    label: "유튜버 맛집",
+  },
+];
+
+export const UTILITY_TAGS = [
+  {
+    iconUrl: "/images/register/tag-utility-1.png",
+    name: "GROUP_RESERVATION",
+    label: "단체 예약",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-2.png",
+    name: "LARGE_PARKING",
+    label: "넓은 주차장",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-3.png",
+    name: "CLEAN_RESTROOM",
+    label: "깔끔한 화장실",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-4.png",
+    name: "PET_FRIENDLY",
+    label: "반려동물 동반 가능",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-5.png",
+    name: "LATE_NIGHT",
+    label: "늦게까지 영업",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-6.png",
+    name: "NEAR_SUBWAY",
+    label: "지하철과 가까운",
+  },
+  {
+    iconUrl: "/images/register/tag-utility-7.png",
+    name: "MANY_NEARBY_ATTRACTIONS",
+    label: "주변에 놀거리가 많은",
+  },
+];
+
+export const ALL_TAGS = [...ATMOSPHERE_TAGS, ...UTILITY_TAGS];
