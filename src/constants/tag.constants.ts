c9c59d827d2e687a744854791472f753486bf014
seojunhwan <seojunhwan@kakao.com>
2025-08-17 21:22:08 +0900

    feat: 태그 이미지 경로 업데이트 및 타입 정의 추가

--- src/constants/tag.constants.ts
+++ src/constants/tag.constants.ts
@@ -1,41 +1,47 @@
+export type Tag = {
+  iconUrl: string;
+  name: string;
+  label: string;
+};
+
 export const ATMOSPHERE_TAGS = [
   {
-    iconUrl: "/images/register/tag-atmosphere-1.png",
+    iconUrl: "/images/tags/OLD_STORE_MOOD.png",
     name: "OLD_STORE_MOOD",
     label: "노포 감성",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-2.png",
+    iconUrl: "/images/tags/ENERGETIC.png",
     name: "ENERGETIC",
     label: "활기찬",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-3.png",
+    iconUrl: "/images/tags/INSTAGRAMMABLE.png",
     name: "INSTAGRAMMABLE",
     label: "인스타 감성",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-4.png",
+    iconUrl: "/images/tags/QUIET.png",
     name: "QUIET",
     label: "조용한",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-5.png",
+    iconUrl: "/images/tags/GOOD_FOR_DRINKING.png",
     name: "GOOD_FOR_DRINKING",
     label: "술 땡기는",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-6.png",
+    iconUrl: "/images/tags/GOOD_FOR_DATING.png",
     name: "GOOD_FOR_DATING",
     label: "데이트하기 좋은",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-7.png",
+    iconUrl: "/images/tags/GOOD_FOR_FAMILY.png",
     name: "GOOD_FOR_FAMILY",
     label: "부모님과 가기 좋은",
   },
   {
-    iconUrl: "/images/register/tag-atmosphere-8.png",
+    iconUrl: "/images/tags/YOUTUBE_FAMOUS.png",
     name: "YOUTUBE_FAMOUS",
     label: "유튜버 맛집",
   },
@@ -43,37 +49,37 @@ export const ATMOSPHERE_TAGS = [
 
 export const UTILITY_TAGS = [
   {
-    iconUrl: "/images/register/tag-utility-1.png",
+    iconUrl: "/images/tags/GROUP_RESERVATION.png",
     name: "GROUP_RESERVATION",
     label: "단체 예약",
   },
   {
-    iconUrl: "/images/register/tag-utility-2.png",
+    iconUrl: "/images/tags/LARGE_PARKING.png",
     name: "LARGE_PARKING",
     label: "넓은 주차장",
   },
   {
-    iconUrl: "/images/register/tag-utility-3.png",
+    iconUrl: "/images/tags/CLEAN_RESTROOM.png",
     name: "CLEAN_RESTROOM",
     label: "깔끔한 화장실",
   },
   {
-    iconUrl: "/images/register/tag-utility-4.png",
+    iconUrl: "/images/tags/PET_FRIENDLY.png",
     name: "PET_FRIENDLY",
     label: "반려동물 동반 가능",
   },
   {
-    iconUrl: "/images/register/tag-utility-5.png",
+    iconUrl: "/images/tags/LATE_NIGHT.png",
     name: "LATE_NIGHT",
     label: "늦게까지 영업",
   },
   {
-    iconUrl: "/images/register/tag-utility-6.png",
+    iconUrl: "/images/tags/NEAR_SUBWAY.png",
     name: "NEAR_SUBWAY",
     label: "지하철과 가까운",
   },
   {
-    iconUrl: "/images/register/tag-utility-7.png",
+    iconUrl: "/images/tags/MANY_NEARBY_ATTRACTIONS.png",
     name: "MANY_NEARBY_ATTRACTIONS",
     label: "주변에 놀거리가 많은",
   },
