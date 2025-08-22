b2a24c9cac79211846d26cc7be3c35e0c2dce4dd
seojunhwan <seojunhwan@kakao.com>
2025-08-22 23:34:03 +0900

    style: FloatingButton 컴포넌트의 하단 위치를 안전 영역을 고려하여 수정

--- src/components/ui/FloatingButton/FloatingButton.css.ts
+++ src/components/ui/FloatingButton/FloatingButton.css.ts
@@ -6,7 +6,7 @@ export const floatingButton = recipe({
   base: {
     position: "fixed",
     right: "calc((100vw - min(100vw, 480px)) / 2 + 1rem)",
-    bottom: "6.4rem",
+    bottom: "calc(6.5rem + env(safe-area-inset-bottom, 0))",
     borderRadius: "4rem",
     zIndex: 1000,
     boxShadow: "0 0 24px 0 rgba(79, 79, 79, 0.24)",
